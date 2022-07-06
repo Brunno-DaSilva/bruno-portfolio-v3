e550d34e902653744f25781b873a770633b03fe7
Bruno DaSilva <hi.dasilvab@gmail.com>
2022-07-06 13:01:29 -0500

    Adding sanity client and initial confi files to reactfodler

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import ProfilePicture from "../../assets/images/UKRAINE.PNG.png";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home Component</h1>
+    </div>
+  );
+};
+
+export default Home;
