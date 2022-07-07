4080bad8071dbde8d65206e1af74e78918e3c0e9
Bruno DaSilva <hi.dasilvab@gmail.com>
2022-07-07 10:02:35 -0500

    Added style to the header section

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Tilt from "react-tilt";
 import { motion } from "framer-motion";
 
-import ProfilePicture from "../../assets/images/Profile_pic_sq_two_noBG.png";
+import ProfilePicture from "../../assets/images/bruno4.png";
 
 import "./Header.scss";
 
@@ -14,17 +14,19 @@ const Header = () => {
         transition={{ duration: 0.8 }}
         className="header__img_container"
       >
-        <Tilt
-          className="Tilt"
-          options={{ max: 20, perspective: 1000, speed: 100 }}
-          style={{ height: 500, width: 500 }}
-        >
-          <img
-            className="Tilt-inner"
-            src={ProfilePicture}
-            alt="Bruno profile img"
-          />
-        </Tilt>
+        <div className="header__img_layer">
+          <Tilt
+            className="Tilt"
+            options={{ max: 20, perspective: 1000, speed: 100 }}
+            style={{ height: 520, width: 520 }}
+          >
+            <img
+              className="Tilt-inner"
+              src={ProfilePicture}
+              alt="Bruno profile img"
+            />
+          </Tilt>
+        </div>
       </motion.div>
       <motion.div
         whileInView={{ x: [200, 0], opacity: [0, 1] }}
@@ -32,13 +34,32 @@ const Header = () => {
         className="header__info"
       >
         <div className="header__tagline">
-          <h1>HelloWorld</h1>
-          <div>
+          <div className="header__tagline-title">
+            <h1>HelloWorld</h1>
+          </div>
+          <div className="header__tagline-description">
             <p>
               <span>My Name is Bruno DaSilva.</span>
               <br />I am a Software Engineer based in Dallas, Texas. <br /> I
               have creative and inquisitive mind, which is key <br /> to
               pursuing my passion for coding.
+            </p>
+          </div>
+
+          <div className="header__tagline-buttons">
+            <p className="zero-margin">
+              <a href="#">Resume</a>
+            </p>
+            <div className="pipe"></div>
+
+            <p>
+              <a href="#">Projects</a>
+            </p>
+
+            <div className="pipe"></div>
+
+            <p>
+              <a href="#">Contact</a>
             </p>
           </div>
         </div>
