0f3d293397915fc4eed2574c685363c59a5652f0
Bruno DaSilva <hi.dasilvab@gmail.com>
2022-07-29 21:43:49 -0500

    Added page name

--- src/assets/data/static-data-pages.js
+++ src/assets/data/static-data-pages.js
@@ -1,21 +1,21 @@
 export const STATIC_DATA_PAGES = [
   {
-    page: "404 page",
+    page_name: "404 page",
     page_ID: "404",
     page_title: "Error",
   },
   {
-    page: "About Me",
+    page_name: "About Me",
     page_ID: "2001",
     page_title: "Hello World",
   },
   {
-    page: "Resume",
+    page_name: "Resume",
     page_ID: "2002",
     page_title: "Resume",
   },
   {
-    page: "Projects",
+    page_name: "Projects",
     page_ID: "2003",
     page_title: "Projects",
   },
